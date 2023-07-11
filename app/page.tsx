79de80d50ba408dba0f87940db4fffa84e4710fa
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-07-11 14:01:07 +0900

    [add]HomeButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "../components/Sidebar";
 import FollowBar from "../components/FollowBar";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
           <div className="col-span-3 lg:col-span-2 boeder-x-1 border-neutral-800 text-sky-500">
-            Hello World
+            <Header showBackArrow label="Home" />
           </div>
           <FollowBar />
         </div>
