12ce4d209c44517d6513aa4399a2d08bd59826f5
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-07-13 19:53:39 +0900

    [add]modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 import Sidebar from "../components/Sidebar";
 import FollowBar from "../components/FollowBar";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-black">
-      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
-        <div className="grid grid-cols-4 h-full">
-          <Sidebar />
-          <div className="col-span-3 lg:col-span-2 boeder-x-1 border-neutral-800 text-sky-500">
-            <Header showBackArrow label="Home" />
+    <>
+      <div className="h-screen bg-black">
+        <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+          <div className="grid grid-cols-4 h-full">
+            <Sidebar />
+            <div className="col-span-3 lg:col-span-2 boeder-x-1 border-neutral-800 text-sky-500">
+              <Header showBackArrow label="Home" />
+              {/* <Modal actionLabel="Submit" isOpen title="Test Modal" />; */}
+            </div>
+            <FollowBar />
           </div>
-          <FollowBar />
         </div>
       </div>
-    </div>
+    </>
   );
 }
